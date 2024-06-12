b9b57d2b450b39939d022b8c97b9838c85fbece2
root <root@Sneh>
2024-06-12 11:31:43 +0530

    small change

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 # CUSTOM
-python Train_promix.py --validation --model_name convnext_nano --noise_type worst --cosine --dataset custom --num_class 100 --rho_range 0.5,0.5 --tau 0.99 --pretrain_ep 1 --warmup_ep 1 --num_epochs 1 --noise_mode custom
+# python Train_promix.py --validation --model_name convnext_nano --noise_type worst --cosine --dataset custom --num_class 100 --rho_range 0.5,0.5 --tau 0.99 --pretrain_ep 1 --warmup_ep 1 --num_epochs 1 --noise_mode custom
 # Try pretrain epochs 0
 
 
-# python Train_promix.py --lr 0.05 --model_name convnext_nano --cosine --dataset custom --num_class 100 --rho_range 0.7,0.7 --tau 0.95 --pretrain_ep 1 --warmup_ep 0 --num_epochs 1 --debias_output 0.5 --debias_pl 0.5  --noise_mode sym --noise_rate 0.2
+# Cifar 100 dataset - same settings as the example by Promix developers
+python Train_promix.py --validation --lr 0.05 --model_name convnext_nano --cosine --dataset custom --num_class 100 --rho_range 0.7,0.7 --tau 0.95 --pretrain_ep 1 --warmup_ep 0 --num_epochs 1 --debias_output 0.5 --debias_pl 0.5  --noise_mode sym --noise_rate 0.2
 # NOTE: naming is very confusing
 # pretrain_ep = warmup epochs
 # warmup_ep = parameter rampup
